f69eb8fa16de0c0f6c82bd6cc15d10dde90c6602
Vashisht Madhavan <vashisht@airbears2-10-142-36-23.airbears2.1918.berkeley.edu>
2015-11-30 16:58:28 -0800

    added API information to send restock notification to phone

--- wear/src/main/java/com/example/baoxie/tips/SupplyOptionsActivity.java
+++ wear/src/main/java/com/example/baoxie/tips/SupplyOptionsActivity.java
@@ -6,9 +6,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.wearable.MessageApi;
+import com.google.android.gms.wearable.Node;
+import com.google.android.gms.wearable.NodeApi;
+import com.google.android.gms.wearable.Wearable;
+
 public class SupplyOptionsActivity extends WearableActivity {
     private Button restockButton;
     private Button locateButton;
+    private GoogleApiClient mApiClient;
+    private static final String RESTOCK = "/restock";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +25,22 @@ public class SupplyOptionsActivity extends WearableActivity {
 
         restockButton = (Button) findViewById(R.id.restock_button);
         locateButton = (Button) findViewById(R.id.locate_button);
+
+         mApiClient = new GoogleApiClient.Builder( this )
+                .addApi( Wearable.API )
+                .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+                    @Override
+                    public void onConnected(Bundle connectionHint) {
+                        /* Successfully connected */
+                    }
+
+                    @Override
+                    public void onConnectionSuspended(int cause) {
+                        /* Connection was interrupted */
+                    }
+                })
+                .build();
+        mApiClient.connect();
 
         restockButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,4 +58,18 @@ public class SupplyOptionsActivity extends WearableActivity {
             }
         });
     }
+
+     public void sendRestockMessage() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String temp = "Product needs to be restocked."
+                NodeApi.GetConnectedNodesResult nodes = Wearable.NodeApi.getConnectedNodes(mApiClient).await();
+                for (Node node : nodes.getNodes()) {
+                    MessageApi.SendMessageResult result = Wearable.MessageApi.sendMessage(
+                            mApiClient, node.getId(), RESTOCK, temp.getBytes()).await();
+                }
+            }
+        }).start();
+     }
 }
